35d697b6789d48bc192f6a79b02fdeb150c75603
rafael.morett@gmail.com <rafael.morett@gmail.com>
2013-01-26 22:54:24 -0200

    Criaçao da classe Ataque.

---
+++ War/War/Ataque.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace War
+{
+    public class Ataque
+    {
+        Territorio atacante;
+        Territorio defensor;
+        int tropas;
+
+        public Ataque(Territorio atacante, Territorio defensor, int tropas)
+        {
+            this.atacante = atacante;
+            this.defensor = defensor;
+            this.tropas = tropas;
+        }
+
+        public Territorio getAtacante()
+        {
+            return atacante;
+        }
+
+        public Territorio getDefensor()
+        {
+            return defensor;
+        }
+    }
+}
